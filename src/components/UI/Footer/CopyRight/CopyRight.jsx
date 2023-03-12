4823e08beb8a662b1a73d710af9c63b023af5bf2
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-12 22:44:32 +0100

    Created footer CopyRight

---
+++ src/components/UI/Footer/CopyRight/CopyRight.jsx
@@ -0,0 +1,39 @@
+import { NavLink } from 'react-router-dom';
+import { Link, Typography } from '@mui/material';
+
+const Copyright = (props) => {
+  const authors = [
+    { name: 'Jakub Jankowski', url: 'https://github.com/TheMankel' },
+    { name: 'Wojciech Bielawa', url: 'https://github.com/HiImWhite' },
+  ];
+
+  return (
+    <Typography
+      variant='body2'
+      color='text.secondary'
+      align='center'
+      padding={2}
+      {...props}>
+      {'Copyright © '}
+      {authors.map((author, i) => (
+        <Link
+          key={i}
+          underline='none'
+          component={NavLink}
+          color='inherit'
+          to={author.url || '/'}
+          sx={{
+            '&:hover': {
+              opacity: 0.75,
+            },
+          }}>
+          {author.name}
+          {authors.length !== i + 1 ? ', ' : ' '}
+        </Link>
+      ))}
+      {new Date().getFullYear()}
+    </Typography>
+  );
+};
+
+export default Copyright;
