6ab25beeed924dfedcceefbb310f7607c51b8c81
HiImWhite <wojttasek1999@gmail.com>
2023-03-12 18:29:24 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,7 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
@@ -18,7 +10,7 @@ import { Container } from '@mui/material';
 
 const navItems = ['Home', 'About', 'Screenshots', 'Faq', 'Contact'];
 
-function Navbar(props) {
+function Navbar() {
   return (
     <AppBar component='nav'>
       <Container maxWidth='xl'>
