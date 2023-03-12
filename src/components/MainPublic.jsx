ce523f788ce6a9eb9d1dea0d2689b5b78cb1074a
HiImWhite <wojttasek1999@gmail.com>
2023-03-12 18:28:29 +0100

    Create MainPublic.jsx

---
+++ src/components/MainPublic.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+
+const MainPublic = () => {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+};
+
+export default MainPublic;
