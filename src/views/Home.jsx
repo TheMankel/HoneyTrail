4ac1fba4fe3b658d27c891320107618ffb4929d3
HiImWhite <wojttasek1999@gmail.com>
2023-03-12 18:28:21 +0100

    Update Home.jsx

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import MainPublic from '../components/MainPublic';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <MainPublic />
+    </>
+  );
 };
 
 export default Home;
