8d695a49e3f26f690f8a7fb81a924638e50916fc
HiImWhite <wojttasek1999@gmail.com>
2023-03-13 16:01:30 +0100

    Initial content

--- src/components/Content/HomeContent/HomeContent.jsx
+++ src/components/Content/HomeContent/HomeContent.jsx
@@ -3,25 +3,61 @@ import { Box, Typography, Container } from '@mui/material';
 
 const Home = () => {
   return (
-    <Box
-      sx={{
-        backgroundSize: 'cover',
-        backgroundImage: `url(${'public/assets/Cinematic.png'})`,
-        height: '100vh',
-      }}>
-      <Container maxWidth='xl' sx={{ height: '100vh' }}>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '50%',
-          }}>
-          <Typography fontSize={96} fontWeight={700} color='#ffd50f'>
-            Honey Trail
-          </Typography>
-        </Box>
-      </Container>
+    <Box>
+      <Box
+        sx={{
+          backgroundSize: 'cover',
+          backgroundImage: `url(${'public/assets/TreeCinematic.png'})`,
+          height: '100vh',
+          opacity: 0.8,
+        }}>
+        <Container maxWidth='xl' sx={{ height: '100vh' }}>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '50%',
+            }}>
+            <Typography fontSize={96} fontWeight={700} color='#ffd50f'>
+              Honey Trail
+            </Typography>
+          </Box>
+        </Container>
+      </Box>
+
+      <Box
+        sx={{
+          backgroundSize: 'cover',
+          backgroundImage: `url(${'public/assets/RightCam.png'})`,
+          height: '100vh',
+          opacity: 0.8,
+        }}>
+        <Container maxWidth='xl' sx={{ height: '100vh' }}>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              height: '100%',
+              flexDirection: 'row',
+            }}>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <Typography varaiant='h1'>Hey</Typography>
+            </Box>
+            <Box
+              component='img'
+              sx={{
+                height: '30%',
+                width: '40%',
+                boxShadow: 20,
+              }}
+              alt='Ingame camera.'
+              src='assets/Cinematic.png'
+            />
+          </Box>
+        </Container>
+      </Box>
     </Box>
   );
 };
