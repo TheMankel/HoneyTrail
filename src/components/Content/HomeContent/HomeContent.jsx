93085ebaa19a8cb9cf28f685d28421a94aa33221
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-13 19:10:43 +0100

    Update HomeContent.jsx

--- src/components/Content/HomeContent/HomeContent.jsx
+++ src/components/Content/HomeContent/HomeContent.jsx
@@ -1,64 +1,12 @@
-import React from 'react';
-import { Box, Typography, Container } from '@mui/material';
+import Welcome from './Welcome/Welcome';
+import Download from './Download/Download';
 
 const Home = () => {
   return (
-    <Box>
-      <Box
-        sx={{
-          backgroundSize: 'cover',
-          backgroundImage: `url(${'public/assets/TreeCinematic.png'})`,
-          height: '100vh',
-          opacity: 0.8,
-        }}>
-        <Container maxWidth='xl' sx={{ height: '100vh' }}>
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: '50%',
-            }}>
-            <Typography fontSize={96} fontWeight={700} color='#ffd50f'>
-              Honey Trail
-            </Typography>
-          </Box>
-        </Container>
-      </Box>
-
-      <Box
-        sx={{
-          backgroundSize: 'cover',
-          backgroundImage: `url(${'public/assets/RightCam.png'})`,
-          height: '100vh',
-          opacity: 0.8,
-        }}>
-        <Container maxWidth='xl' sx={{ height: '100vh' }}>
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              height: '100%',
-              flexDirection: 'row',
-            }}>
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-              <Typography varaiant='h1'>Hey</Typography>
-            </Box>
-            <Box
-              component='img'
-              sx={{
-                height: '30%',
-                width: '40%',
-                boxShadow: 20,
-              }}
-              alt='Ingame camera.'
-              src='assets/Cinematic.png'
-            />
-          </Box>
-        </Container>
-      </Box>
-    </Box>
+    <>
+      <Welcome />
+      <Download />
+    </>
   );
 };
 
