48ab11db657c2d593b889e458ee3e2b16882a8e8
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-14 08:48:09 +0100

    Created basic theme

---
+++ src/theme/theme.js
@@ -0,0 +1,24 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: '#162315',
+    },
+    primary: {
+      main: '#677117',
+    },
+    secondary: {
+      main: '#677117',
+    },
+    text: {
+      primary: '#f7ce0c',
+      secondary: '#ffbc03',
+    },
+  },
+  typography: {
+    fontFamily: ['Gluten'],
+  },
+});
+
+export default theme;
