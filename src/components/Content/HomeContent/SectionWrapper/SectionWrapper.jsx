7c4947bd623325d687cfdf27b17fecc7bbe89d52
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-16 22:11:37 +0100

    Added flex. Fixed bgcolor

--- src/components/Content/HomeContent/SectionWrapper/SectionWrapper.jsx
+++ src/components/Content/HomeContent/SectionWrapper/SectionWrapper.jsx
@@ -2,7 +2,15 @@ import { Box, Container } from '@mui/material';
 
 const HomeWrapper = ({ wrapperStyles, children }) => {
   return (
-    <Box component='section' padding={2} sx={wrapperStyles}>
+    <Box
+      component='section'
+      padding={2}
+      display='flex'
+      flexDirection='column'
+      justifyContent='center'
+      alignItems='center'
+      bgcolor='background.default'
+      sx={wrapperStyles}>
       <Container maxWidth='xl'>{children}</Container>
     </Box>
   );
