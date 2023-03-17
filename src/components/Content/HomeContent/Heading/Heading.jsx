33814399df6beb0f909a2088394e5e506b89cf35
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-17 11:48:22 +0100

    Minor refactor

--- src/components/Content/HomeContent/Heading/Heading.jsx
+++ src/components/Content/HomeContent/Heading/Heading.jsx
@@ -3,6 +3,34 @@ import { Box, Typography } from '@mui/material';
 const Heading = ({ title, subtitle, headingStyles, children }) => {
   if (!title || !subtitle) return null;
 
+  const titleStyles = {
+    textAlign: {
+      xs: 'center',
+      md: 'left',
+    },
+    fontSize: {
+      xs: 32,
+      md: 40,
+      lg: 48,
+    },
+  };
+
+  const subtitleStyles = {
+    textAlign: {
+      xs: 'center',
+      md: 'left',
+    },
+    margin: {
+      xs: '0 auto',
+      md: '0',
+    },
+    fontSize: {
+      xs: 18,
+      md: 24,
+      lg: 30,
+    },
+  };
+
   return (
     <Box sx={headingStyles}>
       <Typography
@@ -10,39 +38,14 @@ const Heading = ({ title, subtitle, headingStyles, children }) => {
         fontWeight={700}
         color='text.primary'
         gutterBottom
-        sx={{
-          textAlign: {
-            xs: 'center',
-            md: 'left',
-          },
-          fontSize: {
-            xs: 32,
-            md: 40,
-            lg: 48,
-          },
-        }}>
+        sx={titleStyles}>
         {title}
       </Typography>
       <Typography
         color='text.secondary'
-        fontSize={24}
         fontWeight={500}
         maxWidth={400}
-        sx={{
-          textAlign: {
-            xs: 'center',
-            md: 'left',
-          },
-          margin: {
-            xs: '0 auto',
-            md: '0',
-          },
-          fontSize: {
-            xs: 18,
-            md: 24,
-            lg: 30,
-          },
-        }}>
+        sx={subtitleStyles}>
         {subtitle}
       </Typography>
       {children}
