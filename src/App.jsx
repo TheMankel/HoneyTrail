3fd2f4ae90e15959c1e503372a69432246c3aeec
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-17 11:51:15 +0100

    Added skeleton views and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
 import Content from './components/Content/Content';
 import Home from './views/Home';
+import FAQ from './views/FAQ';
+import Contact from './views/Contact';
+import PrivacyPolicy from './views/PrivacyPolicy';
 
 function App() {
   return (
     <Routes>
       <Route element={<Content />}>
         <Route path='/' element={<Home />} />
+        <Route path='/faq' element={<FAQ />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/privacy-policy' element={<PrivacyPolicy />} />
       </Route>
     </Routes>
   );
