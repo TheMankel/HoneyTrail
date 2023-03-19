1bcba9d8f1add8a8e1d084ffd951a877abfa4c5a
HiImWhite <wojttasek1999@gmail.com>
2023-03-19 20:05:02 +0100

    Enabling Not Found to false path

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './views/Home';
 import FAQ from './views/FAQ';
 import Contact from './views/Contact';
 import PrivacyPolicy from './views/PrivacyPolicy';
+import NotFound from './views/NotFound';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/faq' element={<FAQ />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/privacy-policy' element={<PrivacyPolicy />} />
+        <Route path='/*' element={<NotFound />} />
       </Route>
     </Routes>
   );
