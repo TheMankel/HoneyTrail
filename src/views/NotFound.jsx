f246f92d5b6abbf82ff2218d3c9b6393033a0366
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-20 10:11:58 +0100

    Update NotFound.jsx

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -8,56 +8,56 @@ import Container from '@mui/material/Container';
 
 const NotFound = () => {
   return (
-    <>
-      <Box
-        bgcolor={'background.paper'}
-        height={'100vh'}
-        display={'flex'}
-        alignItems={'center'}
-        padding={'16px 0 '}>
-        <Container>
-          <Box>
-            <Typography
-              variant='h1'
-              component={'h1'}
-              align={'center'}
-              sx={{ fontWeight: 700 }}>
-              404
-            </Typography>
-            <Typography
-              variant='h6'
-              component='p'
-              color='textSecondary'
-              align={'center'}>
-              Oops! Looks like you followed a bad link.
-              <br />
-              If you think this is a problem with us, please{' '}
-              <Link
-                component={NavLink}
-                to={'/contact'}
-                underline='none'
-                sx={{
-                  '&:hover': {
-                    opacity: 0.75,
-                  },
-                }}>
-                tell us
-              </Link>
-            </Typography>
-            <Box marginTop={4} display={'flex'} justifyContent={'center'}>
-              <Button
-                component={NavLink}
-                variant='contained'
-                color='primary'
-                size='large'
-                to={'/'}>
-                Back home
-              </Button>
-            </Box>
+    <Box
+      bgcolor={'background.paper'}
+      height={'calc(100vh - 64px - 102px - 16px)'}
+      display={'flex'}
+      alignItems={'center'}
+      // padding={'16px 0 '}
+    >
+      <Container>
+        <Box>
+          <Typography
+            variant='h1'
+            component={'h1'}
+            align={'center'}
+            sx={{ fontWeight: 700 }}>
+            404
+          </Typography>
+          <Typography
+            variant='h6'
+            component='p'
+            color='textSecondary'
+            align={'center'}
+            maxWidth='430px'
+            mx='auto'>
+            Oops! Looks like you followed a bad link
+            <br />
+            If you think this is a problem with us, please{' '}
+            <Link
+              component={NavLink}
+              to={'/contact'}
+              underline='none'
+              sx={{
+                '&:hover': {
+                  opacity: 0.75,
+                },
+              }}>
+              tell us
+            </Link>
+          </Typography>
+          <Box marginTop={4} display={'flex'} justifyContent={'center'}>
+            <Button
+              component={NavLink}
+              variant='contained'
+              size='large'
+              to={'/'}>
+              Back home
+            </Button>
           </Box>
-        </Container>
-      </Box>
-    </>
+        </Box>
+      </Container>
+    </Box>
   );
 };
 
