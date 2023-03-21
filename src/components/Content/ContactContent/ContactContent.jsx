7de1ade6d930ac4f83fff6ff36804eed12f97607
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-21 18:44:03 +0100

    Create ContactContent.jsx

---
+++ src/components/Content/ContactContent/ContactContent.jsx
@@ -0,0 +1,28 @@
+import { Box, Grid, Container } from '@mui/material';
+import ContactAside from './ContactAside/ContactAside';
+import ContactForm from './ContactForm/ContactForm';
+
+const ContactContent = () => {
+  return (
+    <Box bgcolor='background.olive'>
+      <Container>
+        <Grid container spacing={8} p={4}>
+          <Grid item container justifyContent={'center'} xs={12} md={6}>
+            <ContactAside />
+          </Grid>
+          <Grid
+            item
+            container
+            alignItems={'center'}
+            justifyContent={'center'}
+            xs={12}
+            md={6}>
+            <ContactForm />
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+};
+
+export default ContactContent;
