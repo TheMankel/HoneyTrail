e99d3f432e3bf1957d5df7c04b09d1260a22af39
HiImWhite <wojttasek1999@gmail.com>
2023-03-21 20:57:53 +0100

    Cleaning code

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,64 +1,8 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Link from '@mui/material/Link';
-import Container from '@mui/material/Container';
+import NotFoundContent from '../components/Content/NotFoundContent/NotFoundContent';
 
 const NotFound = () => {
-  return (
-    <Box
-      bgcolor={'background.paper'}
-      height={'calc(100vh - 64px - 102px - 16px)'}
-      display={'flex'}
-      alignItems={'center'}
-      // padding={'16px 0 '}
-    >
-      <Container>
-        <Box>
-          <Typography
-            variant='h1'
-            component={'h1'}
-            align={'center'}
-            sx={{ fontWeight: 700 }}>
-            404
-          </Typography>
-          <Typography
-            variant='h6'
-            component='p'
-            color='textSecondary'
-            align={'center'}
-            maxWidth='430px'
-            mx='auto'>
-            Oops! Looks like you followed a bad link
-            <br />
-            If you think this is a problem with us, please{' '}
-            <Link
-              component={NavLink}
-              to={'/contact'}
-              underline='none'
-              sx={{
-                '&:hover': {
-                  opacity: 0.75,
-                },
-              }}>
-              tell us
-            </Link>
-          </Typography>
-          <Box marginTop={4} display={'flex'} justifyContent={'center'}>
-            <Button
-              component={NavLink}
-              variant='contained'
-              size='large'
-              to={'/'}>
-              Back home
-            </Button>
-          </Box>
-        </Box>
-      </Container>
-    </Box>
-  );
+  return <NotFoundContent />;
 };
 
 export default NotFound;
