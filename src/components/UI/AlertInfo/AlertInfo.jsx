57cf66d2f78d742b5d1e0225814e01897173168f
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-25 17:20:21 +0100

    Create AlertInfo.jsx

---
+++ src/components/UI/AlertInfo/AlertInfo.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant='filled' {...props} />;
+});
+
+const AlertInfo = (props) => {
+  const { open, handleOpen, severity, message } = props;
+
+  const handleClose = (_, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    handleOpen(false);
+  };
+
+  return (
+    <Snackbar
+      anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      open={open}
+      autoHideDuration={4000}
+      onClose={handleClose}>
+      <Alert onClose={handleClose} severity={severity} sx={{ width: '100%' }}>
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+};
+
+export default AlertInfo;
