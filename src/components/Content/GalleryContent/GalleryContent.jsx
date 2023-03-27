b527a5d32b368cda0dcf1ce52a99c3d2a1882560
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-27 20:36:47 +0200

    Update GalleryContent.jsx

--- src/components/Content/GalleryContent/GalleryContent.jsx
+++ src/components/Content/GalleryContent/GalleryContent.jsx
@@ -1,7 +1,4 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
@@ -10,13 +7,12 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { useTheme } from '@mui/material/styles';
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8];
+const cards = [1, 2, 3, 4, 5, 6];
 
 const GalleryContent = () => {
   const theme = useTheme();
   return (
     <>
-      {/* Hero unit */}
       <Box
         sx={{
           bgcolor: 'background.paper',
@@ -46,18 +42,10 @@ const GalleryContent = () => {
         <Container sx={{ py: 8 }} maxWidth='lg'>
           <Grid container spacing={4}>
             {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={3}>
-                <Card
-                  sx={{
-                    height: '100%',
-                    display: 'flex',
-                    flexDirection: 'column',
-                  }}>
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card>
                   <CardMedia
                     component='img'
-                    sx={{
-                      pt: '56.25%',
-                    }}
                     image='https://source.unsplash.com/random'
                     alt='random'
                   />
@@ -70,9 +58,6 @@ const GalleryContent = () => {
                       the content.
                     </Typography>
                   </CardContent>
-                  <CardActions>
-                    <Button size='small'>View</Button>
-                  </CardActions>
                 </Card>
               </Grid>
             ))}
