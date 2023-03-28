20f3bc6380d26a9dd219620598e0be15fd6d8767
HiImWhite <wojttasek1999@gmail.com>
2023-03-28 14:15:34 +0200

    Create GalleryCards.jsx

---
+++ src/components/Content/GalleryContent/GalleryCards/GalleryCards.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Grid from '@mui/material/Grid';
+import { useTheme } from '@mui/material/styles';
+
+const GalleryCards = () => {
+  const theme = useTheme();
+
+  const cards = [
+    {
+      id: 1,
+      path: 'assets/Gallery/ForestView.png',
+      heading: 'The Forest',
+      content: 'Bear running through the forest',
+    },
+    {
+      id: 2,
+      path: 'assets/Gallery/BearRun.png',
+      heading: 'Gameplay',
+      content: 'Third person view of the bear',
+    },
+    {
+      id: 3,
+      path: 'assets/Gallery/TopView.png',
+      heading: 'From the sky',
+      content: 'Overall look at the generated map',
+    },
+    {
+      id: 4,
+      path: 'assets/Gallery/BearJump.png',
+      heading: 'Jumping Bear',
+      content: 'User interface and ability to jump',
+    },
+    {
+      id: 5,
+      path: 'assets/Gallery/FrontView.png',
+      heading: 'Shadow run',
+      content: 'The darker part of the map',
+    },
+    {
+      id: 6,
+      path: 'assets/Gallery/GameOver.png',
+      heading: 'Game Over',
+      content: 'End of the game',
+    },
+  ];
+
+  return (
+    <Box bgcolor={theme.palette.background.olive}>
+      <Container sx={{ py: 8 }} maxWidth='lg'>
+        <Grid container spacing={4} sx={{ height: '100%' }}>
+          {cards.map((card) => (
+            <Grid item key={card.id} xs={12} sm={6} md={4}>
+              <Card sx={{ height: '100%' }}>
+                <CardMedia
+                  component='img'
+                  image={card.path}
+                  alt='random'
+                  sx={{ height: '70%' }}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant='h5' component='h2'>
+                    {card.heading}
+                  </Typography>
+                  <Typography>{card.content}</Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </Box>
+  );
+};
+
+export default GalleryCards;
