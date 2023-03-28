319b344043503a14700e08ebfb861d7d58dde806
HiImWhite <wojttasek1999@gmail.com>
2023-03-28 14:15:40 +0200

    Refactoring component

--- src/components/Content/GalleryContent/GalleryContent.jsx
+++ src/components/Content/GalleryContent/GalleryContent.jsx
@@ -1,16 +1,9 @@
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { useTheme } from '@mui/material/styles';
-
-const cards = [1, 2, 3, 4, 5, 6];
+import GalleryCards from './GalleryCards/GalleryCards';
 
 const GalleryContent = () => {
-  const theme = useTheme();
   return (
     <>
       <Box
@@ -38,32 +31,7 @@ const GalleryContent = () => {
           </Typography>
         </Container>
       </Box>
-      <Box bgcolor={theme.palette.background.olive}>
-        <Container sx={{ py: 8 }} maxWidth='lg'>
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card>
-                  <CardMedia
-                    component='img'
-                    image='https://source.unsplash.com/random'
-                    alt='random'
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant='h5' component='h2'>
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe
-                      the content.
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-        </Container>
-      </Box>
+      <GalleryCards />
     </>
   );
 };
