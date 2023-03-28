c216adf28fe3a17603c8cc9866eb279aff782956
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-28 15:32:50 +0200

    Update GalleryContent.jsx

--- src/components/Content/GalleryContent/GalleryContent.jsx
+++ src/components/Content/GalleryContent/GalleryContent.jsx
@@ -2,37 +2,28 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import GalleryCards from './GalleryCards/GalleryCards';
+import Heading from '../../UI/Heading/Heading';
 
 const GalleryContent = () => {
   return (
-    <>
-      <Box
-        sx={{
-          bgcolor: 'background.paper',
-          pt: 8,
-          pb: 6,
-        }}>
-        <Container maxWidth='md'>
-          <Typography
-            component='h1'
-            variant='h2'
-            align='center'
-            color='text.primary'
-            gutterBottom>
-            Gallery
-          </Typography>
-          <Typography
-            variant='h4'
-            align='center'
-            color='text.secondary'
-            paragraph>
-            Get a glimpse of the fast-paced action and stunning graphics in our
-            game gallery. Join the fun and show off your bear skills today!
-          </Typography>
-        </Container>
-      </Box>
-      <GalleryCards />
-    </>
+    <Box p={4}>
+      <Container>
+        <Box py={2} borderRadius={1} bgcolor='background.paper'>
+          <Heading
+            title='Gallery'
+            subtitle='Get a glimpse of the fast-paced action and stunning graphics in our
+        game gallery. Join the fun and show off your bear skills today!'
+            headingStyles={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexDirection: 'column',
+            }}
+          />
+        </Box>
+        <GalleryCards />
+      </Container>
+    </Box>
   );
 };
 
