95ba15b84be1ef5f623235fef8360f9719a30483
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-03-28 19:27:37 +0200

    Small faq changes

--- src/components/Content/FaqContent/FaqSection/FaqSection.jsx
+++ src/components/Content/FaqContent/FaqSection/FaqSection.jsx
@@ -6,10 +6,10 @@ const FaqSection = () => {
   const data = [
     {
       title: 'Who are we?',
-      description: 'We are young developers looking for fame!',
+      description: 'We are young developers doing what we love!',
     },
     {
-      title: 'How much does it cost? How long is the game?',
+      title: 'How much does the game cost? How long is it?',
       description:
         'It is totally free! The length of each game is absolutely up to you.',
     },
@@ -19,18 +19,18 @@ const FaqSection = () => {
         'No. Our site does not use any cookies or any other tracking technologies.',
     },
     {
-      title: 'What languages will the game be localized in?',
-      description: 'Everything is written in English.',
+      title: 'In what languages is the game available?',
+      description: 'Everything is in English.',
     },
     {
       title: 'Did you make everything in the game yourself?',
       description:
-        'Not everything, since We license assets from talented artists to speed up production. We modified models in Blender and created the game in Unreal Engine 4.',
+        'Not everything, since We license assets from talented artists to speed up production. We modified some models in Blender and created the game in Unreal Engine 4.',
     },
     {
       title: 'Will you bring the game to IOS?',
       description:
-        'Maybe one day. For now you can only play it on Android devices.',
+        'Hopefully one day. For now, it can only be played on Android devices.',
     },
   ];
 
